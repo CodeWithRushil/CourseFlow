011ed7c8fd88a178b115cca47538ff4e0d08978f
CodeWithRushil <workwithrushil@gmail.com>
2026-01-15 21:53:42 +0530

    check-errors

--- next.config.mjs
+++ next.config.mjs
@@ -17,7 +17,7 @@ const nextConfig = {
     ],
   },
   compiler: {
-    removeConsole: process.env.NODE_ENV === "production",
+    // removeConsole: process.env.NODE_ENV === "production",
   },
 };
 
